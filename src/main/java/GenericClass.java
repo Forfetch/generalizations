97c9a5c5bd964c221f6095d47b1d7293b80c8f04
mabuttta <mabutta@gmail.com>
2023-05-10 20:02:37 +0300

    ver 1.1

--- src/main/java/GenericClass.java
+++ src/main/java/GenericClass.java
@@ -1,2 +1,15 @@
-public class GenericClass {
+import java.util.Arrays;
+
+public class GenericClass <T extends Number>{
+    private T[] array;
+
+    public GenericClass(T[] array) {
+        this.array = array;
+    }
+    public void printSum() {
+        double sum = Arrays.stream(array)
+                .mapToDouble(Number::doubleValue)
+                .sum();
+        System.out.println("Sum of array elements: " + sum);
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        Integer [] array1 = {1,2,3,4,5};
+        GenericClass<Integer> intArray = new GenericClass<>(array1);
+        intArray.printSum();
+
+        Double [] array2 = {1.1,2.2,3.3,4.4,5.5};
+        GenericClass<Double> doubleArray = new GenericClass<>(array2);
+        doubleArray.printSum();
+    }
 }
